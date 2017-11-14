8c97b0f21fa9f067da0d4e1f48da51516457296f
John Griffith <john.griffith@live.co.uk>
2017-11-14 11:10:16 +0000

    Demo read image

--- Visual/Program.cs
+++ Visual/Program.cs
@@ -8,7 +8,17 @@ namespace Visual
     {
         public static void Main(string[] args)
         { 
-            Console.WriteLine("hi");
+            // Instantiates a client
+            var client = ImageAnnotatorClient.Create();
+            // Load the image file into memory
+            var image = Image.FromFile("funny.jpg");
+            // Performs label detection on the image file
+            var response = client.DetectText(image);
+            foreach (var annotation in response)
+            {
+                if (annotation.Description != null)
+                    Console.WriteLine(annotation.Description);
+            }
         }
     }
 }
